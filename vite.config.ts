c4992282f0041a2d3ff012a6e31900b0d59fc405
Stas Basov <bogatyreva.sonechka@yandex.ru>
2024-11-14 23:45:06 +0300

    v_6

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Lighthouse_v_2/',
   plugins: [react()],
 });
-
